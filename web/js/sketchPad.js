cb1d956a44bdf241c6a5989435d74cc607b2a2e6
Fatih Gozenc <fatihgozenc@gmail.com>
2023-04-19 21:05:36 +0200

    listeners done

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -15,33 +15,65 @@ class SketchPad {
         this.canvas.width = size;
         this.canvas.setAttribute("style", `background-color: white; box-shadow: 0px 0px 10px 2px black;`);
         container.appendChild(this.canvas);
-        this.ctx = this.canvas.getContext("2d");
+        const lineBreak = document.createElement("br");
+        container.appendChild(lineBreak);
+        this.undoBtn = document.createElement("button");
+        this.undoBtn.innerHTML = "UNDO";
+        container.appendChild(this.undoBtn);
+        const context = this.canvas.getContext("2d");
+        if (!context) {
+            throw `Context is null. Exiting.`;
+        }
+        this.ctx = context;
+        __classPrivateFieldGet(this, _SketchPad_instances, "m", _SketchPad_redraw).call(this);
         __classPrivateFieldGet(this, _SketchPad_instances, "m", _SketchPad_addEventListeners).call(this);
     }
 }
 _SketchPad_instances = new WeakSet(), _SketchPad_addEventListeners = function _SketchPad_addEventListeners() {
+    if (!this.canvas) {
+        return;
+    }
     this.canvas.onmousedown = (evt) => {
-        this.paths.push([__classPrivateFieldGet(this, _SketchPad_instances, "m", _SketchPad_getMouse).call(this, evt)]);
+        const mouse = __classPrivateFieldGet(this, _SketchPad_instances, "m", _SketchPad_getMouse).call(this, evt);
+        this.paths.push([mouse]);
         this.isDrawing = true;
     };
     this.canvas.onmousemove = (evt) => {
         if (this.isDrawing) {
             const lastPath = this.paths[this.paths.length - 1];
-            lastPath.push(__classPrivateFieldGet(this, _SketchPad_instances, "m", _SketchPad_getMouse).call(this, evt));
+            const mouse = __classPrivateFieldGet(this, _SketchPad_instances, "m", _SketchPad_getMouse).call(this, evt);
+            lastPath.push(mouse);
             __classPrivateFieldGet(this, _SketchPad_instances, "m", _SketchPad_redraw).call(this);
         }
     };
     this.canvas.onmouseup = () => (this.isDrawing = false);
     this.canvas.ontouchstart = (evt) => {
         const loc = evt.touches[0];
-        if (loc && this.canvas) {
-            this.canvas.onmousedown(loc);
-        }
+        // @ts-ignore
+        this.canvas.onmousedown(loc);
+    };
+    this.canvas.ontouchmove = (evt) => {
+        const loc = evt.touches[0];
+        // @ts-ignore
+        this.canvas.onmousemove(loc);
+    };
+    this.canvas.ontouchend = () => {
+        // @ts-ignore
+        this.canvas.onmouseup();
+    };
+    this.undoBtn.onclick = () => {
+        this.paths.pop();
+        __classPrivateFieldGet(this, _SketchPad_instances, "m", _SketchPad_redraw).call(this);
     };
 }, _SketchPad_redraw = function _SketchPad_redraw() {
-    var _a;
-    (_a = this.ctx) === null || _a === void 0 ? void 0 : _a.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     draw.paths(this.ctx, this.paths);
+    if (this.paths.length > 0) {
+        this.undoBtn.disabled = false;
+    }
+    else {
+        this.undoBtn.disabled = true;
+    }
 }, _SketchPad_getMouse = function _SketchPad_getMouse(evt) {
     const rect = this.canvas.getBoundingClientRect();
     return [
--- web/ts/sketchPad.ts
+++ web/ts/sketchPad.ts
@@ -1,8 +1,10 @@
 class SketchPad {
   public canvas: HTMLCanvasElement;
-  public ctx: CanvasRenderingContext2D | null;
+  public ctx: CanvasRenderingContext2D;
   public paths: number[][][] = [];
-  public isDrawing = false;
+  private isDrawing = false;
+  public undoBtn: HTMLButtonElement;
+
   constructor(container: HTMLElement, size = 400) {
     this.canvas = document.createElement("canvas");
     this.canvas.height = size;
@@ -12,20 +14,38 @@ class SketchPad {
       `background-color: white; box-shadow: 0px 0px 10px 2px black;`
     );
     container.appendChild(this.canvas);
-    this.ctx = this.canvas.getContext("2d");
+
+    const lineBreak = document.createElement("br");
+    container.appendChild(lineBreak);
+
+    this.undoBtn = document.createElement("button");
+    this.undoBtn.innerHTML = "UNDO";
+    container.appendChild(this.undoBtn);
+
+    const context = this.canvas.getContext("2d");
+    if (!context) {
+      throw `Context is null. Exiting.`;
+    }
+    this.ctx = context;
+    this.#redraw();
     this.#addEventListeners();
   }
 
   #addEventListeners() {
+    if (!this.canvas) {
+      return;
+    }
     this.canvas.onmousedown = (evt) => {
-      this.paths.push([this.#getMouse(evt)]);
+      const mouse = this.#getMouse(evt);
+      this.paths.push([mouse]);
       this.isDrawing = true;
     };
 
     this.canvas.onmousemove = (evt) => {
       if (this.isDrawing) {
         const lastPath = this.paths[this.paths.length - 1];
-        lastPath.push(this.#getMouse(evt));
+        const mouse = this.#getMouse(evt);
+        lastPath.push(mouse);
         this.#redraw();
       }
     };
@@ -34,15 +54,35 @@ class SketchPad {
 
     this.canvas.ontouchstart = (evt) => {
       const loc = evt.touches[0];
-      if (loc && this.canvas) {
-        //
-      }
+      // @ts-ignore
+      this.canvas.onmousedown(loc);
+    };
+
+    this.canvas.ontouchmove = (evt) => {
+      const loc = evt.touches[0];
+      // @ts-ignore
+      this.canvas.onmousemove(loc);
+    };
+
+    this.canvas.ontouchend = () => {
+      // @ts-ignore
+      this.canvas.onmouseup();
+    };
+
+    this.undoBtn.onclick = () => {
+      this.paths.pop();
+      this.#redraw();
     };
   }
 
   #redraw() {
-    this.ctx?.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     draw.paths(this.ctx, this.paths);
+    if (this.paths.length > 0) {
+      this.undoBtn.disabled = false;
+    } else {
+      this.undoBtn.disabled = true;
+    }
   }
 
   #getMouse(evt: MouseEvent) {
